855992a2269479c5372250e44b76f1a547051805
vidatonkova <vidatonkova@ufl.edu>
2020-02-27 20:07:44 -0500

    added a comment

--- app/src/main/java/com/example/workersafety/MainActivity.java
+++ app/src/main/java/com/example/workersafety/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 //hello
+    //uf team tech for the win
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
