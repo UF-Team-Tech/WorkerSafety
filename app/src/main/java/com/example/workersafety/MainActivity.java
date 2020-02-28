a9fe368ea144552e94454c48989ad7c460bba296
vidatonkova <vidatonkova@ufl.edu>
2020-02-27 20:03:55 -0500

    updated

--- app/src/main/java/com/example/workersafety/MainActivity.java
+++ app/src/main/java/com/example/workersafety/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+//hello
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
